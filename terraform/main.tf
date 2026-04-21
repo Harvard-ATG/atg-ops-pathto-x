c855e67ea05309a95354e0d23e730d67e7cf94eb
Jeremy Guillette <3651377+jaguillette@users.noreply.github.com>
2026-04-21 10:12:46 -0400

    lock down S3 bucket further

--- terraform/main.tf
+++ terraform/main.tf
@@ -114,28 +114,12 @@ resource "aws_s3_bucket" "pathto_static_website_s3_bucket" {
   bucket = "pathto-x"
 }
 
-resource "aws_s3_bucket_ownership_controls" "pathto_static_website_s3_bucket" {
-  bucket = aws_s3_bucket.pathto_static_website_s3_bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
 resource "aws_s3_bucket_public_access_block" "pathto_static_website_s3_bucket" {
   bucket                  = aws_s3_bucket.pathto_static_website_s3_bucket.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_acl" "pathto_static_website_s3_bucket" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.pathto_static_website_s3_bucket,
-    aws_s3_bucket_public_access_block.pathto_static_website_s3_bucket,
-  ]
-  bucket = aws_s3_bucket.pathto_static_website_s3_bucket.id
-  acl    = "public-read"
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_policy" "pathto_static_website_s3_bucket" {
@@ -145,19 +129,19 @@ resource "aws_s3_bucket_policy" "pathto_static_website_s3_bucket" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid       = "PublicRead"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = ["s3:GetObject"]
-        Resource  = ["arn:aws:s3:::pathto-x/*"]
+        Sid    = "AllowCloudFront"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = ["s3:GetObject"]
+        Resource = ["${aws_s3_bucket.pathto_static_website_s3_bucket.arn}/*"]
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.s3_distribution.arn
+          }
+        }
       }
     ]
   })
-}
-
-resource "aws_s3_bucket_website_configuration" "pathto_static_website_s3_bucket" {
-  bucket = aws_s3_bucket.pathto_static_website_s3_bucket.id
-  index_document {
-    suffix = "index.html"
-  }
 }
